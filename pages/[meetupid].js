59f05a63cb96780ebdedea8604f49c3ef00e0570
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-09-04 12:29:12 +0600

    fallback edit

--- pages/[meetupid].js
+++ pages/[meetupid].js
@@ -22,7 +22,7 @@ export async function getStaticPaths() {
   client.close()
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: { meetupid: meetup._id.toString() }
     }))
